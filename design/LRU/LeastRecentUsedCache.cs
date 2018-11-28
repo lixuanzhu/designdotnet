55fbcad8dd32829e1ffb527378ea473bf0e199a6
Sean Zhu <seanzhu@microsoft.com>
2018-11-28 09:25:36 -0800

    lru

---
+++ design/LRU/LeastRecentUsedCache.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.LRU
+{
+    class LeastRecentUsedCache
+    {
+        private int cap;
+        private Dictionary<int, LinkedNode> map;
+        private LinkedNode first;
+        private LinkedNode last;
+
+        public LeastRecentUsedCache(int capacity)
+        {
+            cap = capacity;
+            map = new Dictionary<int, LinkedNode>();
+        }
+
+        public int Get(int key)
+        {
+            if (!map.ContainsKey(key))
+            {
+                return -1;
+            }
+            LinkedNode node = map[key];
+            MoveToFirst(node);
+            return node.Value;
+        }
+
+        private void MoveToFirst(LinkedNode node)
+        {
+            if (node == first)
+            {
+                return;
+            }
+
+            node.Pre.Next = node.Next;
+            if (node != last)
+            {
+                node.Next.Pre = node.Pre;
+            }
+            else
+            {
+                last = node.Pre;
+            }
+
+            node.Next = first;
+            first.Pre = node;
+            first = node;
+        }
+        public void Set(int key, int value)
+        {
+            if (map.ContainsKey(key))
+            {
+                LinkedNode node = map[key];
+                MoveToFirst(node);
+                node.Value = value;
+                return;
+            }
+
+            LinkedNode newNode = new LinkedNode(key, value);
+            newNode.Next = first;
+            first = newNode;
+            map.Add(key, newNode);
+            if (map.Count == 1)
+            {
+                last = newNode;
+            }
+
+            if (map.Count > cap)
+            {
+                map.Remove(last.Key);
+                last = last.Pre;
+                last.Next = null;
+            }
+        }
+
+        static void Test()
+        {
+            LeastRecentUsedCache test = new LeastRecentUsedCache(2);
+            Console.WriteLine(test.Get(3));
+        }
+    }
+
+    class LinkedNode
+    {
+        public int Key { get; set; }
+        public int Value { get; set; }
+        public LinkedNode Next { get; set; }
+        public LinkedNode Pre { get; set; }
+        public LinkedNode(int key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
